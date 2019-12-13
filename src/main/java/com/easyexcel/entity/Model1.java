923ed4fd77d3c1207874696440a9cb63073280f1
zhouhuan <zhouhuan@100doc.com.cn>
2019-12-13 14:18:49 +0800

    创建表头实例

--- src/main/java/com/easyexcel/entity/Model1.java
+++ src/main/java/com/easyexcel/entity/Model1.java
@@ -1,6 +1,21 @@
 package com.easyexcel.entity;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+import lombok.Builder;
+import lombok.Data;
 
-public class Model1 {
+@Data
+@Builder
+public class Model1 extends BaseRowModel {
+    @ExcelProperty(value = "姓名", index = 0)
+    private String name;
+
+    @ExcelProperty(value = "密码", index = 1)
+    private String password;
+
+    @ExcelProperty(value = "年龄", index = 2)
+    private Integer age;
+
 
 }
--- src/main/java/com/easyexcel/entity/Model2.java
+++ src/main/java/com/easyexcel/entity/Model2.java
@@ -1,4 +1,23 @@
 package com.easyexcel.entity;
 
-public class Model2 {
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Model2 extends BaseRowModel {
+    @ExcelProperty(value = "订单号", index = 0)
+    private String orderNo;
+
+    @ExcelProperty(value = "创建人", index = 1)
+    private String name;
+
+    @ExcelProperty(value = "创建时间", index = 2)
+    private LocalDateTime createTime;
+
+
 }
